e9863aa98a82df2f65c61775652011a7f380c671
Boatster45 <johnathankissee@gmail.com>
2024-02-02 15:22:13 -0600

    minor fix for lab2

---
+++ notATypewriter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+printf("************************************************\n");
+printf("*              johnathan.kissee84              *\n");
+printf("*                 Cybersecurity                *\n");
+printf("*         Blinn College @ Bryan Campus         *\n");
+printf("************************************************\n");
+return 0;
+}
+
+
