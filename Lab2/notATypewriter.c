9c9759d1a809d1c42dac6f4923a0ffd7c6e16a56
Boatster45 <johnathankissee@gmail.com>
2024-02-02 15:22:39 -0600

    fix

--- Lab2/notATypewriter.c
+++ Lab2/notATypewriter.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-printf("*************************************************\n");
-printf("*              johnathan.kissee84               *\n");
-printf("*                 Cybersecurity                 *\n");
-printf("*         Blinn College @ Bryan Campus          *\n");
-printf("*************************************************\n");
+printf("************************************************\n");
+printf("*              johnathan.kissee84              *\n");
+printf("*                 Cybersecurity                *\n");
+printf("*         Blinn College @ Bryan Campus         *\n");
+printf("************************************************\n");
 return 0;
 }
 
