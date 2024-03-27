85c66c961f1b1042a5943d8bfd54b1809cded164
Boatster45 <johnathankissee@gmail.com>
2024-03-27 13:06:34 -0500

    lab 5 core

---
+++ Lab5/towerOfHanoi.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "towerOfHanoi.dat"
+
+char* read_colors() {
+
+}
+
+double calc_diameter() {
+
+}
+
+double calc_circumference(){
+
+}
+
+double calc_total_surface_area(){
+    
+}
+
+double calc_volume(){
+    
+}
+
+double calc_toh_disk_surface_area(){
+    
+}
+
+double calc_toh_disk_volume() {
+    
+}
