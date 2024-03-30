5ece9f938fd19f2cd7683b912ed96efbc674cebe
Johnathan Kissee <johnathankissee@gmail.com>
2024-03-30 18:47:53 -0500

    Completed Lab 5, no bonus points yet.

--- Lab5/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-void main () {
-    printf("Hello World!");
-    return 0;
-
-
-}
--- Lab5/towerOfHanoi.h
+++ Lab5/towerOfHanoi.h
@@ -1,32 +1,57 @@
+/*
+Johnathan Kissee
+johnathan.kissee84
+Lab 5: Calculate Size of Tower of Hanoi Disks
+04/01/24  
+COSC 1420 Section N01
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "towerOfHanoi.dat"
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
-char* read_colors() {
+void read_colors(const char *fileName, char *colors_string, int SIZE) {
+    FILE *file = fopen(fileName, "r"); // Open file
+    colors_string[0] = '\0'; // Initialize colors string
+    char color[100]; //char buffer array
+    //while loop to read each line, strconcatenate the color, and add space to the end of each
+    while (fscanf(file, "%s", color) != EOF) {
+        strcat(colors_string, color);
+        strcat(colors_string, " ");
+    }
+
+    fclose(file); //Close file
 
 }
 
-double calc_diameter() {
-
+//Calculate diameter using given radius
+double calc_diameter(double radius) {
+    return 2 * radius;
 }
 
-double calc_circumference(){
-
+//Caclulate Circumference using given radius and M_PI
+double calc_circumference(double radius) {
+    return 2 * M_PI * radius;
 }
 
-double calc_total_surface_area(){
+////Caclulate Total Surface Area of the Cylinder(s)
+double calc_total_surface_area(double radius, double height) {
+    return (2 * M_PI * radius) * (radius + height); //Utilizes the formula: total_area = 2 × π × r × (r + h).
+}
+
+//Caclulate Volume by multiply π by radius^2 and height.
+double calc_volume(double radius, double height) {
+    return M_PI * radius * radius * height;
+}
+
+double calc_toh_disk_surface_area(double radius, double height) {
+    return (2 * M_PI * radius) * (radius + height);
     
 }
 
-double calc_volume(){
-    
-}
-
-double calc_toh_disk_surface_area(){
-    
-}
-
-double calc_toh_disk_volume() {
-    
+double calc_toh_disk_volume(double radius, double height) {
+    return M_PI * radius * radius * height;
 }
